4f3b47577efd5316b3778aacd7c4b564a0fd833f
kareemaboelatta <kareemaboelatta@gmail.com>
2023-09-13 14:12:39 +0300

    add GradientProgressbar

--- app/src/main/java/com/example/kewaapp/home/presentaion/screens/HomeScreen.kt
+++ app/src/main/java/com/example/kewaapp/home/presentaion/screens/HomeScreen.kt
@@ -2,12 +2,17 @@ package com.example.kewaapp.home.presentaion.screens
 
 import androidx.annotation.ColorInt
 import androidx.annotation.DrawableRes
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -23,18 +28,35 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.kewaapp.R
 import com.example.kewaapp.common.ui.common.Dimensions.IconSize
 import com.example.kewaapp.common.ui.common.PaddingDimensions
 import com.example.kewaapp.common.ui.theme.KewaAppTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 @Preview(
@@ -52,6 +74,127 @@ fun HomeScreen() {
 
     }
 }
+
+
+@Preview
+@Composable
+fun GradientProgressbarPrev() {
+    KewaAppTheme{
+        GradientProgressbar()
+    }
+}
+
+
+@Composable
+fun GradientProgressbar(
+    indicatorHeight: Dp = 34.dp,
+    backgroundIndicatorColor: Color = Color.LightGray.copy(alpha = 0.3f),
+    indicatorPadding: Dp = 24.dp,
+    gradientColors: List<Color> = listOf(
+        MaterialTheme.colorScheme.primary,
+        MaterialTheme.colorScheme.secondary,
+
+        MaterialTheme.colorScheme.tertiary,
+
+        ),
+    numberStyle: TextStyle = TextStyle(
+        fontFamily = FontFamily.Monospace,
+        fontSize = 32.sp
+    ),
+    animationDuration: Int = 1000,
+    animationDelay: Int = 0
+) {
+
+    var downloadedPercentage by remember {
+        mutableStateOf(100.0F)
+    }
+
+
+
+    val scope  = rememberCoroutineScope()
+
+    val animateNumber = animateFloatAsState(
+        targetValue = downloadedPercentage,
+        animationSpec = tween(
+            durationMillis = animationDuration,
+            delayMillis = animationDelay
+        ), label = ""
+    )
+
+    LaunchedEffect(Unit) {
+        scope.launch {
+            while (downloadedPercentage < 100){
+                downloadedPercentage+=5
+                delay(200)
+            }
+
+        }
+    }
+
+
+    Column {
+        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Canvas(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(indicatorHeight - 5.dp)
+                .padding(start = indicatorPadding, end = indicatorPadding)
+        ) {
+
+            // Background indicator
+            drawLine(
+                color = backgroundIndicatorColor,
+                cap = StrokeCap.Round,
+                strokeWidth = size.height,
+                start = Offset(x = 0f, y = 0f),
+                end = Offset(x = size.width, y = 0f)
+            )
+
+            // Convert the downloaded percentage into progress (width of foreground indicator)
+            val progress =
+                (animateNumber.value / 100) * size.width // size.width returns the width of the canvas
+
+            // Foreground indicator
+            drawLine(
+                brush = Brush.linearGradient(
+                    colors = gradientColors
+                ),
+                cap = StrokeCap.Round,
+                strokeWidth = size.height,
+                start = Offset(x = 0f, y = 0f),
+                end = Offset(x = progress, y = 0f)
+            )
+
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text(
+            text = downloadedPercentage.toInt().toString() + "%",
+            style = numberStyle
+        )
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @Composable
