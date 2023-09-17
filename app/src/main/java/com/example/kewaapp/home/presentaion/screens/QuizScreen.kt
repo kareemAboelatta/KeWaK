d5cfbee75cfe9fd31543e7eeaf9cdce8c272adff
kareem_aboelatta <kareem.aboelatta@baianat.com>
2023-09-18 01:20:45 +0200

    start pager

--- app/src/main/java/com/example/kewaapp/home/presentaion/screens/QuizScreen.kt
+++ app/src/main/java/com/example/kewaapp/home/presentaion/screens/QuizScreen.kt
@@ -1,6 +1,7 @@
 package com.example.kewaapp.home.presentaion.screens
 
 import android.content.res.Configuration
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -10,9 +11,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,19 +26,36 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.lerp
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.kewaapp.R
 import com.example.kewaapp.common.ui.common.Dimensions.BigIconSize
-import com.example.kewaapp.common.ui.common.Dimensions.IconSize
 import com.example.kewaapp.common.ui.common.PaddingDimensions
 import com.example.kewaapp.common.ui.components.GradientButton
+
 import com.example.kewaapp.common.ui.components.GradientProgressbar
 import com.example.kewaapp.common.ui.components.GradientProgressbarPreview
 import com.example.kewaapp.common.ui.theme.KewaAppTheme
 import com.example.kewaapp.home.data.Quiz
+import kotlin.math.absoluteValue
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Button
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.BaselineShift
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -88,12 +108,43 @@ fun QuizScreen() {
                     modifier = Modifier.padding(10.dp)
                 )
 
-
+                QuizPager()
 
             }
         }
     }
 
+}
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Preview
+@Composable
+fun QuizPager() {
+    val state: PagerState = rememberPagerState ( 1 )
+    val animationScope = rememberCoroutineScope()
+    Column {
+        HorizontalPager(state = state, pageCount = 20) { page ->
+            Card(
+                Modifier
+                    .size(200.dp)
+                    .graphicsLayer {
+                        // Calculate the absolute offset for the current page from the
+                        // scroll position. We use the absolute value which allows us to mirror
+                        // any effects for both directions
+                        val pageOffset = (
+                                (state.currentPage - page) + state
+                                    .currentPageOffsetFraction
+                                ).absoluteValue
+
+                        // We animate the alpha, between 50% and 100%
+                        alpha = 0.5F
+                    }
+            ) {
+                // Card content
+            }
+        }
+    }
 }
 
 
