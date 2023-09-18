c3ac0bf42b1ae8b97335cd1e798e923c0f9cd24d
kareem_aboelatta <kareem.aboelatta@baianat.com>
2023-09-18 07:47:45 +0200

    work on page in quiz

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,9 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
+    implementation ("androidx.compose.ui:ui-util:1.5.1")
+
+
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
 
@@ -79,6 +82,8 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
 
 
     implementation("androidx.compose.material3:material3:1.1.1")
--- app/src/main/java/com/example/kewaapp/home/presentaion/screens/QuizScreen.kt
+++ app/src/main/java/com/example/kewaapp/home/presentaion/screens/QuizScreen.kt
@@ -28,7 +28,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.lerp
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.kewaapp.common.ui.common.Dimensions.BigIconSize
@@ -48,13 +47,18 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PageSize
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Button
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.BaselineShift
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.util.lerp
+import com.example.kewaapp.R
 import kotlinx.coroutines.launch
 
 
@@ -121,28 +125,53 @@ fun QuizScreen() {
 @Preview
 @Composable
 fun QuizPager() {
-    val state: PagerState = rememberPagerState ( 1 )
-    val animationScope = rememberCoroutineScope()
-    Column {
-        HorizontalPager(state = state, pageCount = 20) { page ->
-            Card(
-                Modifier
-                    .size(200.dp)
-                    .graphicsLayer {
-                        // Calculate the absolute offset for the current page from the
-                        // scroll position. We use the absolute value which allows us to mirror
-                        // any effects for both directions
-                        val pageOffset = (
-                                (state.currentPage - page) + state
-                                    .currentPageOffsetFraction
-                                ).absoluteValue
+    val pagerState = rememberPagerState(pageCount = {
+        10
+    })
+    HorizontalPager(
+        modifier = Modifier
+            .fillMaxSize()
 
-                        // We animate the alpha, between 50% and 100%
-                        alpha = 0.5F
-                    }
-            ) {
-                // Card content
+        ,
+
+        state = pagerState,
+        pageSpacing = 5.dp,
+        pageSize = object : PageSize {
+            override fun Density.calculateMainAxisPageSize(
+                availableSpace: Int,
+                pageSpacing: Int
+            ): Int {
+                return ((availableSpace - 1.5 * pageSpacing) * 0.5f).toInt()
             }
+        },
+
+    ) { page ->
+        Card(
+            Modifier
+                .size(300.dp)
+                .graphicsLayer {
+                    // Calculate the absolute offset for the current page from the
+                    // scroll position. We use the absolute value which allows us to mirror
+                    // any effects for both directions
+                    val pageOffset = (
+                            (pagerState.currentPage - page) + pagerState
+                                .currentPageOffsetFraction
+                            ).absoluteValue
+
+                    // We animate the alpha, between 50% and 100%
+                    alpha = lerp(
+                        start = 0.5f,
+                        stop = 1f,
+                        fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                    )
+                }
+        ) {
+            // Card content
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(id = R.drawable.art),
+                contentDescription = null
+            )
         }
     }
 }
